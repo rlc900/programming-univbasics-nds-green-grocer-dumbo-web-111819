7397374d9a48af0f30c918b73af468986e8ceff4
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-13 17:03:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,7 +17,7 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   new_cart = []
   i = 0 
-  while i < cart.length do 
+  while i < cart.length 
     
     i += 1 
   end 
