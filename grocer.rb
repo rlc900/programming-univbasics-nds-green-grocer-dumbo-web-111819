47d9dc9d7b82d0149ea2ddc0c2333b434bf0b262
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-13 17:16:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,7 +25,7 @@ def consolidate_cart(cart)
       new_cart_item = {
         item: cart[i][:item],
         price: cart[i][:price],
-        
+        clearance: cart[i][:clearance]
       }
     end 
     i += 1 
