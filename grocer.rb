71ebcd732d6a651461371a6db6e70bacd25411ce
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-13 17:47:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ def apply_coupons(cart, coupons)
   cart_item_with_coupon = find_item_by_name_in_collection(item_couponed_name, cart)
   if cart_item && cart_item[:count] >= coupons[i][:num]
     #checks if cart_item is in our cart AND if the cart items count is greater than or equal to the number of that item on our coupon. if those two things are true:
-    if 
+    if cart_item_with_coupon
     end 
   end 
   i += 1 
