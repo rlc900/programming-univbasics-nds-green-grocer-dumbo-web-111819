83921d5e152e066dba2b66bc33b42cb8af0e0352
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-13 17:33:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,7 @@ def apply_coupons(cart, coupons)
   cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
   item_couponed_name = "#{coupons[i][:item]} W/COUPON"
   cat_item_with_coupon = find_item_by_name_in_collection(item_couponed_name, cart)
-  if cart
+  if cart_item 
     
   end 
   i += 1 
