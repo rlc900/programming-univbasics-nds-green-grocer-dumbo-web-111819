c383660067abf2ca98a45fdff6015f63d5e6b02b
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-13 17:51:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,7 @@ def apply_coupons(cart, coupons)
     else
       cart_item_with_coupon = {
         item: item_name_couponed,
-        price: 
+        price: coupons[i][:price]
       }
     end 
   end 
