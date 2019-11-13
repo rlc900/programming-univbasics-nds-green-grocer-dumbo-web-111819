d9d1f7bdbf7eed5cb0ec91b255f93664a1f3c4ee
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-13 17:12:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def consolidate_cart(cart)
   i = 0 
   while i < cart.length 
     new_cart_item = find_item_by_name_in_collection(cart[i][:item], new_cart)
-    if new_cart_item 
+    if new_cart_item != nil 
       
     end 
     i += 1 
